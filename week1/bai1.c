ab8b00373d76a152c271a6554b0b971b77c31788
namnt0413 <namnt0413@gmail.com>
2022-04-05 23:11:23 +0700

    asg week1 v2

--- week1/bai1.c
+++ week1/bai1.c
@@ -1,65 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-	#include <stdio.h>
-	#include <stdlib.h>
-	#include <string.h>
+#define MAX_ARRAY 10000
 
+typedef struct word {
+	char str[100];
+	int count;
+} word;
+
+word wordArr[1000];
+
+void setWordArray()
+{
+	int i;
+	for(i = 0; i < MAX_ARRAY; i++)
+		wordArr[i].count = 0;
+}
+
+	int max_word = 0;
+
+void search(char *str)
+{
+	int check=0;
+	int i=0;
+	for(i=0;i<max_word;i++)
+	{
+		if ( strcmp(wordArr[i].str,str ) ==1 )
+		{
+			wordArr[i].count++;
+			check=1;
+		}
+		
+	}
+	
+	if( check == 0)
+	{
+		strcpy( wordArr[max_word].str , str );
+		max_word++;
+	}
+	
+//	printf("%s\n",wordArr[max_word-1].str);
+	// printf("%d : ",max_word);
+	// printf("%d\n",check);
+	
+}
+
+	void defineWord(char *str)
+	{
+		int length = strlen(str);
+		int i = 0;
+		for(i = 0; i < length; i++) {
+			if((str[i] < 'A' || str[i] > 'Z') && (str[i] < 'a' || str[i] > 'z'))
+			{
+				str[i] = ' ';
+			}
+			if((str[i] >= 'A' && str[i] <= 'Z'))
+			{
+				str[i] = str[i] + 32;
+			}
+		}
+	}
 
 	int main(){
+    	char line[100];
 
-    char line[200];
-	    char word[400][10];
-	    char mean[100000];
-	    FILE *fp;
+	    FILE *fp,*fptr;
 	    if((fp = fopen("vanban.txt", "r")) == NULL)
 	    {
 	        printf("Can not open vanban.txt!\n");
 	        return;
 	    }
-	    int i;
-	    int first_word_flag = 0;
-	    int word_count = 0;
-        
-	    /*do
-	    {
+
+        fptr = fopen("newVanban.txt", "w+");
+
+		while (fscanf(fp, "%s", line) == 1)
+		{
+			defineWord(line);
+			fprintf(fptr, "%s ", line);
+			printf("%s\n",line);
+		}
+		fclose(fptr);
+		fclose(fp);
+		
+		if((fp = fopen("newVanban.txt", "r")) == NULL) {
+			printf("Can't not open newVanban.txt!\n");
+		}
+		
+	
+	// while (fscanf(fp, "%s", line) == 1)
+	// {
+	// 	search(line);
+	// 	printf("%s , %d\n",line,max_word);
+	// }
 
 
-	    } */
-        while((fgets(line, 100, fp)) != NULL){
-            // printf("%s\n", line);
-				int j=0,k=0,l=0;
-
-			for(i = 0; i < sizeof(line); i++){
-
-				if(  line[i] != ' ' && line[i] != '\n' && line[i] !=',' && line[i] !='.' && ( line[i] < '0' || line[i] > '9' ) && line[i] != '(' && line[i] != ')' ){
-
-						word[j][k] = line[i];
-						k++;
-	    				first_word_flag = 1;
-					
-				}
-				else{
-					if( first_word_flag==1){
-						j++;
-						first_word_flag = 0;
-					}
-					k=0;
-
-				}
-
-
-
-			}
-
-			for(l = 0; l < j ; l++){
-				printf("%s\n", word[l]);
-			}
-
-
-
-        };
-
-	    fclose(fp);
-
+		return 0;
 	}
 
 
